4d26a551e1d37a3e93a2308ce759bcae054760c3
anant jain <anant.jain@optimusinfo.com>
2013-05-17 11:53:34 +0530

    Adding new files to optimus-qa-jenkins repository

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default["jenkins"]["home_dir"] = ".jenkins"
+default["jenkins"]["url"] = "http://mirrors.jenkins-ci.org/war/latest/jenkins.war"
+default["jenkins"]["mirror"] = "http://updates.jenkins-ci.org"
+default["jenkins"]["plugins"] = ["github-api", "git", "github", "ansicolor", "rake", "greenballs", "html5-notifier-plugin", "openid", "jobConfigHistory", "dashboard-view", "twitter", "email-ext", "sectioned-view", "extended-choice-parameter", "jenkins-multijob-plugin"]
